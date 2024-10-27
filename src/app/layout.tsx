4f494cfe7a4858d1949c33885aedb2ab0c23d8aa
Prashant Kashyap <pkashyap@adobe.com>
2024-10-27 09:25:24 +0530

    resolve lint errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 "use client";
 // import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
 
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from "aws-amplify";
 
 import outputs from "@/amplify_outputs.json";
@@ -12,14 +9,12 @@ import { useAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
-
 export default function App() {
   const { user, signOut } = useAuthenticator();
 
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'</h1>
+      <h1>{user?.signInDetails?.loginId}</h1>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
